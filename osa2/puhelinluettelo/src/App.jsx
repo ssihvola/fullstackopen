f018f8b5db79b994cf61ef33370165db05c7ee50
ssihvola <sihvola@protonmail.com>
2023-08-28 10:53:54 +0300

    2.6 valmis

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -6,19 +6,37 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName
+    }
+
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button onClick={addName}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+        <div>
+          {persons.map((person, index) => (
+            <p key={index}>{person.name}</p>
+          ))}
+        </div>
     </div>
   )
 
