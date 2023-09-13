344622f5554a61d1717314aa1829bdd2665818fb
ssihvola <sihvola@protonmail.com>
2023-09-13 10:20:03 +0300

    tehtävä 3.1 valmis

---
+++ tehtavat-3.1-3.6/index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+
+const persons = [
+  {
+    id: 1,
+    name: "arto hellas",
+    number: "040-123456"
+  },
+  {
+    id: 2,
+    name: "ada lovelace",
+    number: "39-44-5323523"
+  },
+  {
+    id: 3,
+    name: "dan abramov",
+    number: "12-234-34353535"
+  },
+  {
+    id: 4,
+    name: "mary poppendick",
+    number: "390-23234-234234"
+  }
+]
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
