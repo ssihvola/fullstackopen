6d43666974d94c61a49571af70b584075e4ab4ae
ssihvola <sihvola@protonmail.com>
2023-10-26 11:39:56 +0300

    tehtävä 3.15 valmis

--- index.js
+++ index.js
@@ -114,10 +114,16 @@ app.get('/info', (req, res) => {
   res.send(info)
 })
 
-app.delete('/api/persons/:id', (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
   const id = Number(req.params.id)
   persons = persons.filter(person => person.id !== id)
   res.status(204).end()
+
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const PORT = process.env.PORT || 3001
