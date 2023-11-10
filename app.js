8db80dc60d4aa574077d91ea76ffbd50d8beb9d7
ssihvola <sihvola@protonmail.com>
2023-11-10 10:30:26 +0200

    tehtävä 4.2 valmis

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
-const mongoUrl = config.MONGODB_URI
-
-mongoose.connect(mongoUrl)
+mongoose.connect(config.MONGODB_URI)
   .then(() => {
     logger.info('connected to MongoDB')
   })
@@ -17,5 +16,6 @@ mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app
--- index.js
+++ index.js
@@ -4,34 +4,6 @@ const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
