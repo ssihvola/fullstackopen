dfa1a9d74f695926f6cde70d449ced4ed496fb29
ssihvola <sihvola@protonmail.com>
2023-11-14 13:19:22 +0200

    tehtävä 4.4 valmis

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,11 @@ const dummy = (blogs) => {
   return blogs.length - blogs.length + 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
