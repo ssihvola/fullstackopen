457f6abe7bc431c964693352b031c4dfafba37eb
ssihvola <sihvola@protonmail.com>
2024-02-02 10:35:18 +0200

    tehtävä 5.21 valmis

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,25 @@
+Cypress.Commands.add('login', ({ username, password }) => {
+  cy.request('POST', 'http://localhost:3003/api/login', {
+    username: 'sihvola', password: 'salainen'
+  }).then(response => {
+    localStorage.setItem('loggedBlogappUser', JSON.stringify(response.body))
+    cy.visit('')
+  })
+})
+
+Cypress.Commands.add('createBlog', ({ title, author, url }) => {
+  cy.request({
+    url: 'http://localhost:3003/api/blogs',
+    method: 'POST',
+    body: { title, author, url },
+    headers: {
+      'Authorization': `Bearer ${JSON.parse(localStorage.getItem('loggedBlogappUser')).token}`
+    }
+  })
+
+  cy.visit('')
+})
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
