d8bcfde55e95d805b6eb109aaef476e4131a6a03
ssihvola <sihvola@protonmail.com>
2024-02-09 11:13:31 +0200

    tehtävä 6.10 valmis

--- redux-anecdotes/src/reducers/filterReducer.js
+++ redux-anecdotes/src/reducers/filterReducer.js
@@ -1,18 +1,16 @@
-const filterReducer = (state = "", action) => {
-  console.log(action.type)
-  switch (action.type) {
-    case 'SET_FILTER':
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = ""
+
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState,
+  reducers: {
+    filterChange(state, action) {
       return action.payload
-    default:
-      return state
+    }
   }
-}
+})
 
-export const filterChange = filterText => {
-  return {
-    type: 'SET_FILTER',
-    payload: filterText
-  }
-}
-
-export default filterReducer
+export const { filterChange } = filterSlice.actions
+export default filterSlice.reducer
