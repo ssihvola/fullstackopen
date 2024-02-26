245088a1d1d01d05ce39a29dbf7c5af4e7f398e8
ssihvola <sihvola@protonmail.com>
2024-02-26 12:33:59 +0200

    alustetaan tehtävää 7.9

---
+++ query-anecdotes/src/components/Notification.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/prop-types */
+import { useNotificationValue } from "./NotificationContext"
+
+const Notification = () => {
+  const notification = useNotificationValue()
+
+  const style = {
+    border: 'solid',
+    padding: 10,
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  if (notification === null) return null
+
+  return (
+    <div style={style}>
+      {notification}
+    </div>
+  )
+}
+
+export default Notification
